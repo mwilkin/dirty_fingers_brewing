7d9f8679d7ee041d030b401494917fb0c5e6e7e1
Matt Wilkin <mwilkin22@gmail.com>
2016-08-11 15:25:10 -0700

    add new keg funtion.

--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -2,20 +2,22 @@ import { Component, EventEmitter } from 'angular2/core';
 import { KegComponent } from './keg.component';
 import { Keg } from './keg.model';
 import { EditKegDetailsComponent } from './edit-keg-details.component';
+import { NewKegComponent } from './new-keg.component';
 
 @Component({
   selector: 'keg-list',
   inputs: ['kegList'],
   outputs: ['onKegSelect'],
-  directives: [KegComponent, EditKegDetailsComponent],
+  directives: [KegComponent, EditKegDetailsComponent, NewKegComponent],
   template: `
   <keg-display *ngFor="#currentKeg of kegList"
     (click)="kegClicked(currentKeg)"
     [class.selected]="currentKeg === selectedKeg"
     [keg]="currentKeg">
   </keg-display>
-  <edit-keg-details *ngIf="selectedKeg" [keg]="selectedKeg">    
+  <edit-keg-details *ngIf="selectedKeg" [keg]="selectedKeg">
   </edit-keg-details>
+  <new-keg (onSubmitNewKeg)="createKeg($event)"></new-keg>
   `
 })
 export class KegListComponent {
@@ -29,5 +31,10 @@ export class KegListComponent {
 console.log(clickedKeg, "child");
   this.selectedKeg = clickedKeg;
   this.onKegSelect.emit(clickedKeg);
+  }
+  createKeg(name: string): void {
+    this.kegList.push(
+      new Keg(name, this.kegList.length)
+    );
   }
 }
---
+++ app/new-keg.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Keg } from './keg.model';
+
+@Component({
+  selector: 'new-keg',
+  outputs: ['onSubmitNewKeg'],
+  template:`
+  <div class="keg-form">
+    <h3>Create Keg: </h3>
+    <input placeholder="Name" class="col-sm-8 input-lg" #newName>
+    <button (click)="addKeg(newName)" class="btn-success btn-lg add-button">Add</button>
+  </div>
+  `
+})
+export class NewKegComponent {
+  public onSubmitNewKeg: EventEmitter<String>;
+  constructor(){
+    this.onSubmitNewKeg = new EventEmitter();
+  }
+  addKeg(userName: HTMLInputElement){
+    this.onSubmitNewKeg.emit(userName.value);
+    userName.value = "";
+  }
+}
